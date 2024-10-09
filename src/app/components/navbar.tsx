de820befc022b09ee40731cec1f01196a7bb127c
Connor Dirks <cdirks4@me.com>
2024-10-09 00:05:56 -0400

    fix: text color

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
-          <div className="text-lg font-mackinac text-indigo-800 dark:text-indigo-300 font-semibold">
+          <div className="text-lg font-mackinac text-indigo-800 font-semibold">
             <Link href="/" className="text-center group">
               <div className="flex items-center">
                 <svg
