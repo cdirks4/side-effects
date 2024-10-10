942895e63da927daa3232e4fcdac236af140c2f2
Connor Dirks <cdirks4@me.com>
2024-10-10 13:10:14 -0400

    fix: gitroll bug

--- src/app/components/GifDisplay.tsx
+++ src/app/components/GifDisplay.tsx
@@ -1,16 +1,22 @@
-"use client"
-import React, { useState } from 'react';
-import Image from 'next/image';
-import gifPath2 from '../../../public/assets/pip-ai_workflow.gif'; // Your first GIF
-import gifPath1 from '../../../public/assets/standard_workflow.gif';  // Your second GIF (replace with actual path)
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import gifPath2 from "../../../public/assets/pip-ai_workflow.gif"; // Your first GIF
+import gifPath1 from "../../../public/assets/standard_workflow.gif"; // Your second GIF (replace with actual path)
 
-const GifDisplay = ({ altText = 'GIF', width, height }) => {
+const GifDisplay = ({ altText = "GIF", width, height }) => {
   const [currentGif, setCurrentGif] = useState(gifPath1); // Default to the first GIF
   const [isToggled, setIsToggled] = useState(false); // Track the toggle state
 
   const toggleGif = () => {
     setIsToggled(!isToggled);
     setCurrentGif(isToggled ? gifPath1 : gifPath2);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      toggleGif();
+    }
   };
 
   return (
@@ -25,11 +31,18 @@ const GifDisplay = ({ altText = 'GIF', width, height }) => {
       />
       <div className="mt-6">
         <div
-          className={`w-16 h-6 bg-gray-400 rounded-full p-1 cursor-pointer ${isToggled ? 'bg-blue-500' : 'bg-gray-400'}`}
+          className={`w-16 h-6 rounded-full p-1 cursor-pointer ${
+            isToggled ? "bg-blue-500" : "bg-gray-400"
+          }`}
           onClick={toggleGif}
+          onKeyPress={handleKeyPress}
+          role="button"
+          tabIndex={0}
         >
           <div
-            className={`bg-white w-4 h-4 rounded-full shadow-md transform duration-300 ${isToggled ? 'translate-x-10' : 'translate-x-0'}`}
+            className={`bg-white w-4 h-4 rounded-full shadow-md transform duration-300 ${
+              isToggled ? "translate-x-10" : "translate-x-0"
+            }`}
           ></div>
         </div>
       </div>
