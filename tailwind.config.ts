199af907e83f9b48d0bb45675ef96b49f1f318bd
Connor Dirks <cdirks4@me.com>
2024-10-13 14:53:00 -0400

    feat: lower text size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,18 @@ const config: Config = {
       fontFamily: {
         mackinac: ['"P22 Mackinac Pro"', "serif"],
       },
+      fontSize: {
+        xs: "0.65rem",
+        sm: "0.75rem",
+        base: "0.875rem",
+        lg: "1rem",
+        xl: "1.125rem",
+        "2xl": "1.25rem",
+        "3xl": "1.5rem",
+        "4xl": "1.875rem",
+        "5xl": "2.25rem",
+        "6xl": "3rem",
+      },
       colors: {
         indigo: {
           600: "#4f46e5",
